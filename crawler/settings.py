617d409e0575871e9e906fdd624802be78dafd82
D3ror <97361000+D3ror@users.noreply.github.com>
2025-09-09 09:12:22 +0300

    Update settings.py

--- crawler/settings.py
+++ crawler/settings.py
@@ -6,11 +6,3 @@ ROBOTSTXT_OBEY = True
 LOG_LEVEL = "INFO"
 CLOSESPIDER_PAGECOUNT=100
 
-# Playwright integration
-DOWNLOAD_HANDLERS = {
-    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-}
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-PLAYWRIGHT_BROWSER_TYPE = "chromium"
-PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT = 30_000  # ms

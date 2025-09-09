47a9bf270a79b90fa2391dd307caada37d820a49
D3ror <97361000+D3ror@users.noreply.github.com>
2025-09-09 09:14:03 +0300

    Update settings.py

--- crawler/settings.py
+++ crawler/settings.py
@@ -5,4 +5,4 @@ NEWSPIDER_MODULE = "crawler.spiders"
 ROBOTSTXT_OBEY = True
 LOG_LEVEL = "INFO"
 CLOSESPIDER_PAGECOUNT=100
-
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
